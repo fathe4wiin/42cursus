96d8b5cacf500e09f7528b5100b251dbbb218539
Bilal Fathi <bfathi@e2r4p12.1337.ma>
2025-10-18 10:36:57 +0100

    fixed lstsize

--- libft/ft_lstsize_bonus.c
+++ libft/ft_lstsize_bonus.c
@@ -6,7 +6,7 @@
 /*   By: bfathi <bfathi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/14 20:04:23 by fathe4wiin        #+#    #+#             */
-/*   Updated: 2025/09/21 18:20:12 by bfathi           ###   ########.fr       */
+/*   Updated: 2025/10/18 10:36:22 by bfathi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ int	ft_lstsize(t_list *lst)
 
 	if (!lst)
 		return (0);
-	i = 0;
+	i = 1;
 	while (lst->next != NULL)
 	{
-		i++;
 		lst = lst->next;
+		i++;
 	}
 	return (i);
 }
