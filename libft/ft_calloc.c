f4115eaa1753c0391fed8046f8c1fa53d8079bf4
Bilal Fathi <bfathi@e2r3p4.1337.ma>
2025-10-20 21:03:21 +0100

    final commit

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,17 +6,18 @@
 /*   By: bfathi <bfathi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/13 15:51:05 by fathe4wiin        #+#    #+#             */
-/*   Updated: 2025/10/16 22:00:30 by bfathi           ###   ########.fr       */
+/*   Updated: 2025/10/20 18:12:55 by bfathi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdint.h>
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*res;
 
-	if (nmemb != 0 && size > 4294967295 / nmemb)
+	if (nmemb != 0 && size > SIZE_MAX / nmemb)
 		return (NULL);
 	res = malloc(nmemb * size);
 	if (!res)
